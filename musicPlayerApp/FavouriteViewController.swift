2d7a07d0f157bf6afbf5c6e79917b07cb91af97a
Bibin <bibinthomas787@gmail.com>
2019-11-22 13:24:23 -0500

    committed by adding the code for favourites view controller

--- musicPlayerApp/FavouriteViewController.swift
+++ musicPlayerApp/FavouriteViewController.swift
@@ -7,16 +7,91 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+
 
 class FavouriteViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var favouriteTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+         gettingSongsName()
         // Do any additional setup after loading the view.
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+              return songs.count
+              
+          
+          
+          
+      }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+     
+     
+    
+     
+     
+         
+         cell.textLabel?.textColor = #colorLiteral(red: 0, green: 0.1020925418, blue: 0.659463346, alpha: 1)
+            cell.textLabel?.text = songs[indexPath.row]
+         cell.textLabel?.font = UIFont(name: "Comic Sans MS", size: CGFloat())
+         
+     
+        return cell
+    }
+    
+     override func didReceiveMemoryWarning()
+          {
+              super.didReceiveMemoryWarning()
+          }
+    
+     func gettingSongsName(){
+           
+            let folderURL = URL(fileURLWithPath:Bundle.main.resourcePath!)
+                  
+              do
+                  {
+                      let songPath = try FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+                      
+                   
+               
+                      //loop through the found urls
+                      for song in songPath
+                      {
+                          var mySong = song.absoluteString
+                          
+                          if mySong.contains(".mp3")
+                          {
+                              let findString = mySong.components(separatedBy: "/")
+                              mySong = findString[findString.count-1]
+                              mySong = mySong.replacingOccurrences(of: "%20", with: " ")
+                              mySong = mySong.replacingOccurrences(of: ".mp3", with: "")
+                              songs.append(mySong)
+                          }
+                          
+                      }
+                      
+                      favouriteTableView.reloadData()
+                  }
+                  catch
+                  {
+                      print ("ERROR")
+                  }
+           
+          
+       }
+    
+    
     /*
     // MARK: - Navigation
 
